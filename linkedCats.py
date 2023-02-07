a44fcfe35f32c4c20d9a2ff2343450e1f900fded
Anton Sineev <sineevanton@mail.ru>
2023-02-07 17:35:06 +0300

    Comments added. Some mistakes removed

--- linkedCats.py
+++ linkedCats.py
@@ -1,18 +1,22 @@
+# Импорт нужных библиотек
 import math
 import time
 import matplotlib.pyplot as plt
 
+# Коробки для кошек
 class Box:
     def __init__(self, cat=None):
         self.cat = cat
         self.nextCat = None
         self.prevCat = None
 
+# Описание котиков
 class LinkedCats:
     def __init__(self):
         self.head = None
 
     # Переворачиваем котиков
+    # В методе всего один цикл, сложность = O(n)
     def reverse(self):
         temp = None
         current = self.head
@@ -55,15 +59,19 @@ class LinkedCats:
             boxIndex = boxIndex + 1
             lastBox = lastBox.nextCat
 
+    # Выставка кошек
     def showCats(self, cat):
         while cat is not None:
             print(cat.cat, end=" ")
             cat = cat.nextCat
         print()
 
-timeCounts = []
-inputDataCounts = [10, 100, 1000, 10000, 100000]
+timeCounts = [] # Тут будем хранить значения времени, за которое отработала сортировка
+inputDataCounts = [10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000] # Значения количества входных данных (столько котиков создадим)
 
+# Для каждого значения количества входных параметров создаем соответствующее количество котиков
+# переворичиваем их, вычисляя время разворота и записывая его в массив выше
+# Строки закомментированы чтобы не засорять консоль большими выводами
 for el in inputDataCounts:
     catBox = LinkedCats()
     for i in range(el):
@@ -76,15 +84,24 @@ for el in inputDataCounts:
     #catBox.showCats(catBox.head)
     timeCounts.append(end - start)
 
+
 print(*timeCounts)
+times = timeCounts
+# Логика для работы с данными для графиков:
+# Суть - линейно преобразовать значения времени, чтобы не осталось значений < 0
+# так как для таких значений возведение в степень сломает график
 for i in range(len(timeCounts)):
     timeCounts[i] = (int)(timeCounts[i]*1000000)
 print(*timeCounts)
 
-endPoint = max(timeCounts)
+endPoint = max(timeCounts) # Эта точка нужна для искуственной генерации данных для построения графиков сложности
 
-inputValuesCount = [i for i in range((int)(endPoint/len(timeCounts)), endPoint+1, (int)(endPoint/len(timeCounts)))]
-print(inputValuesCount)
+# Искуственные данные (точки), ограниченные максимальным временем работы исследуемого алгоритма, используются
+# Для построения графиков сложности
+
+SinteticData = [i for i in range((int)(endPoint/len(timeCounts)), endPoint+1, (int)(endPoint/len(timeCounts)))]
+print(SinteticData)
+
 # Классы оценок сложности:
 # O(1)
 # O(log(n))
@@ -94,26 +111,28 @@ print(inputValuesCount)
 # O(n^3)
 # O(2^n)
 
-# inputValuesCount = [i for i in range(10, 1000)]
-logData = [math.log(i) for i in inputValuesCount]
-nData = inputValuesCount
-nLogNData = [i*math.log(i) for i in inputValuesCount]
-squareData = [i**2 for i in inputValuesCount]
-cubeData = [i**3 for i in inputValuesCount]
-expData = [2**i for i in inputValuesCount]
+# Данные для графиков сложности
 
+logData = [math.log(i) for i in SinteticData]
+nData = SinteticData
+nLogNData = [i*math.log(i) for i in SinteticData]
+squareData = [i**2 for i in SinteticData]
+cubeData = [i**3 for i in SinteticData]
+expData = [2**i for i in SinteticData]
+
+# Запускаем рисовалку
 fig, ax = plt.subplots()
-ax.set_xlim([2000, 15000])
+# ax.set_xlim([2000, 15000])
 
-l1 = ax.plot(inputValuesCount, logData, label = "O(log(n))")
-l2 = ax.plot(inputValuesCount, nData, label = "O(n)")
-l3 = ax.plot(inputValuesCount, nLogNData, label = "O(n*log(n))")
-l4 = ax.plot(inputValuesCount, squareData, label = "O(n^2)")
-l5 = ax.plot(inputValuesCount, cubeData, label = "O(n^3)")
-#l6 = ax.plot(inputValuesCount, expData, label = "O(2^n)")
-l7 = ax.plot(inputDataCounts, inputValuesCount, label = "User algorithm")
-plt.yscale('log')
+l1 = ax.plot(SinteticData, logData, label = "O(log(n))")
+l2 = ax.plot(SinteticData, nData, label = "O(n)")
+l3 = ax.plot(SinteticData, nLogNData, label = "O(n*log(n))")
+l4 = ax.plot(SinteticData, squareData, label = "O(n^2)")
+l5 = ax.plot(SinteticData, cubeData, label = "O(n^3)")
+#l6 = ax.plot(inputValuesCount, expData, label = "O(2^n)") # Убрал, так как сильно большие числа
+l7 = ax.plot(times, inputDataCounts, label = "User algorithm")
+plt.yscale('log') # Нагляднее получается
 ax.legend(shadow=True, fancybox=True)
-plt.xlabel("common X")
-plt.ylabel("common Y")
+plt.xlabel("Sintetic time (log)")
+plt.ylabel("Input data count (log)")
 plt.show()
