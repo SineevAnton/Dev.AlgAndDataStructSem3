8c7c1fec17b0c00f33ada23cba16c9a7efa2a186
Anton Sineev <sineevanton@mail.ru>
2023-02-07 15:32:55 +0300

    Visualisation of default difficulties added

--- linkedCats.py
+++ linkedCats.py
@@ -1,4 +1,5 @@
-import matplotlib as plt
+import math
+
 
 class Box:
     def __init__(self, cat=None):
@@ -59,6 +60,7 @@ class LinkedCats:
             cat = cat.nextCat
         print()
 
+import matplotlib.pyplot as plt
 
 catBox = LinkedCats()
 for i in range(10):
@@ -68,7 +70,44 @@ for i in range(10):
 # catBox.addCat(4)
 # catBox.addCat(5)
 
+data = [i for i in range(10)]
+
 catBox.showCats(catBox.head)
 catBox.reverse()
 catBox.showCats(catBox.head)
 #print(catBox)
+
+# fig, simpleChart = plt.subplots()
+# simpleChart.plot(data)
+# plt.show()
+
+# Классы оценок сложности:
+# O(1)
+# O(log(n))
+# O(n)
+# O(n*log(n))
+# O(n^2)
+# O(n^3)
+# O(2^n)
+inputValuesCount = [i for i in range(10, 1000)]
+logData = [math.log(i) for i in inputValuesCount]
+nData = inputValuesCount
+nLogNData = [i*math.log(i) for i in inputValuesCount]
+squareData = [i**2 for i in inputValuesCount]
+cubeData = [i**3 for i in inputValuesCount]
+expData = [2**i for i in inputValuesCount]
+
+fig, ax = plt.subplots()
+ax.set_ylim([0, 1000])
+
+l1 = ax.plot(inputValuesCount, logData, label = "O(log(n))")
+l2 = ax.plot(inputValuesCount, nData, label = "O(n)")
+l3 = ax.plot(inputValuesCount, nLogNData, label = "O(n*log(n))")
+l4 = ax.plot(inputValuesCount, squareData, label = "O(n^2)")
+l5 = ax.plot(inputValuesCount, cubeData, label = "O(n^3)")
+l6 = ax.plot(inputValuesCount, expData, label = "O(2^n)")
+#plt.yscale('log')
+ax.legend(shadow=True, fancybox=True)
+plt.xlabel("common X")
+plt.ylabel("common Y")
+plt.show()
